cace3a88b09e6dfa4a83a8e4f67258aa38586a86
WashFall <carl.christensen98@gmail.com>
2021-09-15 10:54:08 +0200

    Added Assignment 2
    The second assignment of the course

---
+++ Assets/Assignment2.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assignment2 : ProcessingLite.GP21
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Background(Color.white);
+        parabolic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int coordX = 0;
+    int coordY = 10;
+
+    void parabolic()
+    {
+        for(int i = 0; i < 11; i++)
+        {
+            Stroke(200, 200, 200);
+
+            if(i % 3 == 2)
+            {
+                Stroke(0, 0, 0);
+            }
+
+            Line(0, coordY - i, coordX + i, 0);
+        }
+    }
+}
