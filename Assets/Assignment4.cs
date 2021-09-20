31c59f042fb70e8e39ad156bb0cacf9afb338dcd
WashFall <carl.christensen98@gmail.com>
2021-09-20 15:57:24 +0200

    Added acceleration
    Fixed acceleration so that it works as intended. Also removed one of the two circles, may add one again at a later date.

--- Assets/Assignment4.cs
+++ Assets/Assignment4.cs
@@ -4,30 +4,42 @@ using UnityEngine;
 
 public class Assignment4 : ProcessingLite.GP21
 {
-    private Vector2 char1, char2;
+    private Vector2 char1;
     private int diameter = 1;
-    private float acc = 1.5f;
-    private float speed = 5f;
+    private float acc = 5f;
+    private float speed = 3f;
+    private float maxSpeed = 10f;
+    private Vector2 input;
 
     public void Start()
     {
-        char1 = new Vector2(Width / 2, 6);
-        char2 = new Vector2(Width / 2, 2);
+        char1 = new Vector2(Width / 2, Height / 2);
     }
 
     public void Update()
     {
         Background(0);
         Circle(char1.x, char1.y, diameter);
-        Circle(char2.x, char2.y, diameter);
         Move();
     }
 
     public void Move()
     {
-        char1.x += speed * Input.GetAxis("Horizontal") * Time.deltaTime;
-        char1.y += speed * Input.GetAxis("Vertical") * Time.deltaTime;
-        char2.x += speed * acc * Input.GetAxis("Horizontal") * Time.deltaTime;
-        char2.y += speed * acc * Input.GetAxis("Vertical") * Time.deltaTime;
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (input.magnitude != 0)
+        {
+            char1.x += speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+            char1.y += speed * Input.GetAxis("Vertical") * Time.deltaTime;
+            if (speed < maxSpeed)
+            {
+                speed += acc * Time.deltaTime;
+            }
+            
+        }
+        else if (input.magnitude == 0)
+        {
+            speed = 3f;
+        }
     }
 }
